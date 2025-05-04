402bf98377dd45fbac630ee37ebbeeba8d8a8b3b
gslcapital <allaisterleslie70@gmail.com>
2025-05-04 21:43:37 +0100

    Delete components directory

--- components/ui/input.jsx
+++
@@ -1,4 +0,0 @@
-
-export function Input({ className, ...props }) {
-  return <input className={`border px-3 py-2 rounded-xl ${className}`} {...props} />;
-}
--- components/ui/textarea.jsx
+++
@@ -1,4 +0,0 @@
-
-export function Textarea({ className, ...props }) {
-  return <textarea className={`border px-3 py-2 rounded-xl ${className}`} {...props} />;
-}
